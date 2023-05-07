2093ce698c487d5d4f99c8d89ecea51f5e459858
Himesh95 <106318032+Himesh9512@users.noreply.github.com>
2023-05-07 17:34:11 +0530

    Change in example CV

--- src/utils/example.ts
+++ src/utils/example.ts
@@ -19,7 +19,7 @@ export const exampleCv: CV = {
 			from: "2019",
 			to: "2022",
 			description:
-				"As a software engineer, I excel at developing innovative solutions using Java, Python, and C++. With expertise in software design, development, testing, and deployment, I am a skilled problem solver who thrives in dynamic, fast-paced environments. I am a collaborative team player with experience in mobile app development and complex enterprise systems.",
+				"Studying for a B.Tech degree at a top university can be a challenging yet rewarding experience. The curriculum is designed to provide students with a strong foundation in engineering principles and practical skills, along with opportunities for hands-on projects and research.",
 		},
 		{
 			id: uuidv4(),
