17c7fc84d975acdaae34eab09044d0962cfbaea9
Himesh9512 <himeshjangid0000@gmail.com>
2023-05-07 14:24:18 +0530

    Create resume preview responsive

--- src/components/cv/Preview.tsx
+++ src/components/cv/Preview.tsx
@@ -14,25 +14,33 @@ interface CvProps {
 const Cv = forwardRef(({ cv }: CvProps, ref) => {
 	const { personalDetails, education, experience }: CV = cv;
 	return (
-		<Box padding={2} component="div" ref={ref}>
-			<Paper
-				elevation={3}
-				sx={{
-					paddingBottom: 20,
-					border: "1px solid black",
-					minWidth: "fit-content",
-				}}>
-				<Header
-					firstName={personalDetails.firstName}
-					lastName={personalDetails.lastName}
-					title={personalDetails.title}
-					email={personalDetails.email}
-					phone={personalDetails.phone}
-				/>
-				<Container>
-					<Body summary={personalDetails.summary} education={education} experience={experience} />
-				</Container>
-			</Paper>
+		<Box
+			margin={2}
+			padding={2}
+			component="div"
+			maxWidth={650}
+			overflow="scroll"
+			border="1px solid black">
+			<Box ref={ref} component="div" minWidth={600}>
+				<Paper
+					elevation={3}
+					sx={{
+						paddingBottom: 10,
+						border: "1px solid black",
+						minWidth: "fit-content",
+					}}>
+					<Header
+						firstName={personalDetails.firstName}
+						lastName={personalDetails.lastName}
+						title={personalDetails.title}
+						email={personalDetails.email}
+						phone={personalDetails.phone}
+					/>
+					<Container>
+						<Body summary={personalDetails.summary} education={education} experience={experience} />
+					</Container>
+				</Paper>
+			</Box>
 		</Box>
 	);
 });
